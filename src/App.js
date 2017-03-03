a06a4481f39d0d872781d1dc537ca77893d716cf
Fredward <faandrade@gmail.com>
2017-03-02 19:22:18 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App tk-adobe-garamond-pro">
+        <h1>Frederico<br/>Andrade</h1>
+        <div id="content">
+          <div id="main">
+            <section>
+              <h2>Experience</h2>
+              <article>
+                <span className="date">
+                  <p>2013 to date <br/> Full-time from 01.2016</p>
+                </span>
+                <span className="body">
+                  <h3><a href="https://www.wikitongues.org">Wikitongues</a></h3>
+                  <p className="description">Co-founder, chief platform officer, board member (volunteer). I crafted the organization’s brand meeting strategic goals, and produced all of the collateral (business cards, pamplhets, presentation materials, ads). I conceived, designed and implemented an open source tool Poly to advance the organziation’s mission, presented it by invitation at the New York Tech Meetup’s respected Hack of the Month segment and raised a record breaking kickstarter campaign to build it and the distributed team of open source contributors that helps me maintain it.</p>
+                </span>
+              </article>
+              <article>
+                <span className="date">
+                  <p>10.2013 to 05.2015</p>
+                </span>
+                <span className="body">
+                  <h3><a href="https://www.amicushq.com">Amicus</a></h3>
+                  <p className="description">Product manager, front-end developer, ui/ux designer. I branded, led product design and strategy, and managed the implementation of Amicus Post, a tool that replaced the core business and was featured at NY Tech Meetup. Amicus Post is a widely used direct action tool for political advocacy and outreach, trusted by Airbnb, the Human Rights Campaign, Greenpeace, Everytown for Gun Safety and many more. Amicus has since rebranded the business to use the visual vocabulary I developed for Amicus Post.</p>
+                </span>
+              </article>
+              <article>
+                <span className="date">
+                  <p>08.2013 to 12.2015</p>
+                </span>
+                <span className="body">
+                  <h3>Additional Contracting</h3>
+                  <p className="description">Design and implementation of branding, product ui/ux and marketing websites.  roughout the last few years, I have taken on a range of contracting opportunities to broaden my horizons and network, and have the  exibility to implement new stylistic approaches and usability feels. Clients include Airtable, Datamonkey, Quotidian Ventures, DV01, Matte, the National Sawdust and Vision Into Art.</p>
+                </span>
+              </article>
+              <article>
+                <div className="body more">...</div>
+              </article>
+            </section>
+            <section>
+              <h2>Awards, Press</h2>
+              <article>
+                <span className="date">
+                  <p>06.2016</p>
+                </span>
+                <span className="body">
+                  <h3>Creators and Creatives</h3>
+                  <p className="description">“In a time when languages are dying out at an unprecedented rate, Frederico Andrade strives to preserve the world’s linguistic diversity.”</p>
+                </span>
+              </article>
+              <article>
+                <span className="date">
+                  <p>02.2016</p>
+                </span>
+                <span className="body">
+                  <h3>Public Radio International</h3>
+                  <p className="description">“ is nonprofit wants to build a tool to share [...] all the world’s languages”</p>
+                </span>
+              </article>
+              <article>
+                <span className="date">
+                  <p>03.2015</p>
+                </span>
+                <span className="body">
+                  <h3>Record breaking Kickstarter campaign</h3>
+                  <p className="description">Most funded campaign in the translation category</p>
+                </span>
+              </article>
+              <article>
+                <span className="date">
+                  <p>06.2014</p>
+                </span>
+                <span className="body">
+                  <h3>New York Tech Meetup</h3>
+                  <p className="description">Hack of the month by invitation for Poly</p>
+                </span>
+              </article>
+              <article>
+                <span className="date">
+                  <p>11.2012</p>
+                </span>
+                <span className="body">
+                  <h3>Wallpaper* Magazine, Lazy Bytes</h3>
+                  <p className="description">“EPFL + ECAL lab pushes our buttons with its ‘lazy bytes’ project”</p>
+                </span>
+              </article>
+              <article>
+                <div className="body more">...</div>
+              </article>
+            </section>
+            <section>
+              <h2>Education</h2>
+              <article>
+                <span className="date">
+                  <p>2009 - 2013</p>
+                </span>
+                <span className="body">
+                  <h3>Parsons, the New School for Design</h3>
+                  <p className="description">Valedictorian, design and technology bfa</p>
+                </span>
+              </article>
+            </section>
+          </div>
+          <div id="side">
+            <section>
+              <h2>Contact</h2>
+              <ul>
+                <li>faandrade@gmail.com</li>
+                <li>1 917 683 8299</li>
+                <li>github.com/fredericoAndrade</li>
+              </ul>
+            </section>
+            <section>
+              <h2>Statement</h2>
+              <p>I was awarded the prestigious O-1 visa recognizing my work at Wikitongues and other NY tech companies. Recent responsibilities include producing Poly, a world-class web app, building and maintaining an open source developer community to support it, and bringing in top talent to the organization’s board of directors. In addition, I implemented creative strategies and partnerships to nurture and edify the organization as it scales.</p>
+            </section>
+            <section>
+              <h2>Disciplines</h2>
+              <ul>
+                <li>lean, agile development cycles</li>
+                <li>product strategy, ui/ux</li>
+                <li>full-stack software development</li>
+                <li>graphic design, editorial</li>
+                <li>concept art, illustration</li>
+                <li>film, stop motion</li>
+              </ul>
+            </section>
+            <section>
+              <h2>Software</h2>
+              <ul>
+                <li></li>
+                <li>rails, postgres, heroku, firebase</li>
+                <li>react.js, es6, jq</li>
+                <li>ember.js, handlebars</li>
+                <li>node, express</li>
+                <li>processing, arduino</li>
+                <li>adobe creative suite, sketch</li>
+                <li>autodesk entertainment suite</li>
+              </ul>
+            </section>
+            <section>
+              <h2>Communications</h2>
+              <ul>
+                <li>portuguese <i>fluent</i></li>
+                <li>french <i>intermediate</i></li>
+                <li>spanish <i>intermediate</i></li>
+                <li>italian <i>basic</i></li>
+                <li>japanese <i>basic</i></li>
+              </ul>
+            </section>
+            <section>
+              <h2>Wikitongues</h2>
+              <p>Wikitongues is an international 501(c)(3) non-profit I founded and am on the board of, dedicated to stopping language loss worldwide by raising awareness about cultural diversity through language, and building tools to for communities everywhere to defend, document and share their languages.</p>
+            </section>
+          </div>
+        </div>
+        <footer>
+          <div className="side">March 2017</div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
