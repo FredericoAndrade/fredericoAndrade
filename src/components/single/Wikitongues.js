004f4ac5cf23ccaae2d1fb483d0817bdc8c9c101
Fredward <faandrade@gmail.com>
2017-04-03 16:46:09 -0400

    adds relevant things to package

--- src/components/single/Wikitongues.js
+++ src/components/single/Wikitongues.js
@@ -45,7 +45,7 @@ export default class Wikitongues extends React.Component {
                 <p>Wikitongues is a movement for language rights. Together with speakers and signers from every language in the world, Wikitongues is raising awareness about linguistic diversity and rallying to defend it</p>
               </section>
               <figure className="two-col">
-                <img src="media/wikitongues/logo/lockups/calligraphic.png" alt=""/>
+                <img src="media/wikitongues/logo/lockups/horizontal.png" alt=""/>
                 <figcaption>The Wikitongues logo represents a human waving their arms</figcaption>
               </figure>
               <section className="one-col">
@@ -55,87 +55,9 @@ export default class Wikitongues extends React.Component {
                 <p>I <em>designed</em> and <em>implemented</em> an open source tool <Link to="/poly">Poly</Link> to advance the organziation’s mission, presented it by invitation at the New York Tech Meetup’s respected <a href="https://vimeo.com/127205159#t=3133s">Hack of the Month</a> segment and <a href="http://kck.st/poly">raised a record breaking kickstarter campaign</a> to build it and the distributed team of open source contributors that helps me maintain it. (They even made us a fancy custom URL '<a href="http://kck.st/poly">kck.st/poly</a>')</p>
               </section>
             </div>
-
-            <h2>Process</h2>
-
-            <div className="block">
-              <section>
-                <h3 id="brand">Brand</h3>
-                <ul>
-                  <li>Colors</li>
-                  <li>Imagery</li>
-                  <li>Grid</li>
-                  <li>Graphics</li>
-                </ul>
-              </section>
-              <section>
-                <h3 id="collateral">Collateral</h3>
-                <ul>
-                  <li>Business cards</li>
-                  <li>Pamphlets</li>
-                  <li>Table banner</li>
-                  <li>Stickers</li>
-                  <li>Mugs</li>
-                  <li>Catalogs</li>
-                </ul>
-              </section>
-              <section>
-                <h3 id="website">Website</h3>
-                <ul>
-                  <li>Version 1</li>
-                  <li>Version 2</li>
-                  <li>Version 3</li>
-                </ul>
-              </section>
-            </div>
           </div>
         </div>
       </div>
     )
   }
 }
-
-/*
-<div className="block">
-  <h1 id="logo">Logo</h1>
-  <p>The Wikitongues logo went through a long iteration process to arrive at the current glyph.</p>
-</div>
-
-<section className="media">
-
-  <h3>Glyph</h3>
-
-  <section className="series three">
-    <img src="media/wikitongues/logo/lockups/mark.png" alt=""/>
-    <img src="media/wikitongues/logo/lockups/calligraphic.png" alt=""/>
-    <video src="media/wikitongues/logo/lockups/sting.mov" autoPlay loop poster="media/wikitongues/logo/calligraphic.png"></video>
-  </section>
-
-  <h3>Lockups</h3>
-
-  <section className="series two">
-    <img src="media/wikitongues/logo/lockups/vertical.png" alt=""/>
-    <img src="media/wikitongues/logo/lockups/horizontal.png" alt=""/>
-  </section>
-
-  <section className="series one"><img src="media/wikitongues/logo/corporateMark.png" alt=""/></section>
-
-</section>
-
-<h3>Early variations</h3>
-
-<section className="series two">
-  <img src="media/wikitongues/logo/variations/1.png" alt=""/>
-  <img src="media/wikitongues/logo/variations/2.png" alt=""/>
-</section>
-
-<section className="series one"><img src="media/wikitongues/logo/variations/4.png" alt=""/></section>
-
-<h3>Human Glyph</h3>
-
-<section className="series three">
-  <img src="media/wikitongues/logo/variations/10.jpg" alt=""/>
-  <img src="media/wikitongues/logo/variations/12.png" alt=""/>
-  <img src="media/wikitongues/logo/variations/11.png" alt=""/>
-</section>
-  */
