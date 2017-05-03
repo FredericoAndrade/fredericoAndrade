3ea92446fc4c1df6150845fca662c7463b790c89
Fredward <faandrade@gmail.com>
2017-05-03 17:31:37 -0400

    poly content

--- src/components/single/Poly.js
+++ src/components/single/Poly.js
@@ -7,6 +7,7 @@ import {
   // Route,
   Link
 } from 'react-router-dom'
+// <p>I built our first prototype in Ember.js and Firebase, and had the opportunity to demonstrate it at the New York Tech Meetup. At that moment, the core functionalities were designed, and the behavior of the app had shaped up.</p>
 
 export default class Poly extends React.Component {
   componentDidMount() {
@@ -35,16 +36,15 @@ export default class Poly extends React.Component {
             <div className="block three-col">
               <h1>Poly</h1>
               <section className="one-col">
-                <p>Poly is open source, modern software to share and learn every language in the world. Recognizing that awareness-raising, while an important layer of the organization's mission, does not go far enough to provide access to the language, <Link to="/wikitongues">Wikitongues</Link> resolved to develop the platform to empower speakers and signers of every language in the world to share document their own languages on an easy and simple platform.</p>
-                <p>The development process for Poly has been long and involved, and as any dynamic technology, will never be complete. I realized from the onset that the only feasible way for Wikitongues to produce technology was if it was done inhouse, removing the financial restrictions. As such, I took it upon myself to learn all of the requisite technologies to develop the application, leveraging my experience with front-end development as an entry point.</p>
+                <p>Poly is open source, modern software to share and learn every language in the world. Recognizing that awareness-raising, while an important layer of the organization's mission, does not go far enough to provide access to the language, <Link to="/wikitongues">Wikitongues</Link> resolved to develop the platform to empower speakers and signers of every language in the world to share document their own languages on an easy and simple platform. The development process for Poly has been long and involved, and as any dynamic technology, will never be complete. I realized from the onset that the only feasible way for Wikitongues to produce technology was if it was done inhouse, removing the financial restrictions. As such, I took it upon myself to learn all of the requisite technologies to develop the application, leveraging my experience with front-end development as an entry point.</p>
               </section>
               <section className="two-col">
                 <p>The application empowers anyone to produce language documentation and lessons, and is being used by Wikitongues to collect introductory phrases in the world's less documented languages.</p>
+                <p>Designed to be used by anyone regardless of familiarity with the platform, close attention was paid to interface affordances and semotic transfer.</p>
                 <figure>
                   <iframe height="354" src="https://www.youtube.com/embed/-fVDgKy7ieA?rel=0&controls=0&showinfo=0" frameBorder="0" allowFullScreen></iframe>
                   <figcaption>A video demo of the Poly app</figcaption>
                 </figure>
-                <p>I built our first prototype in Ember.js and Firebase, and had the opportunity to demonstrate it at the New York Tech Meetup. At that moment, the core functionalities were designed, and the behavior of the app had shaped up.</p>
               </section>
               <span><h3>Product design</h3></span>
               <section className="two-col">
