ee4cd3bd2a4c225b2aee530c6cdc28995bee53fc
Fredward <faandrade@gmail.com>
2017-05-03 17:38:24 -0400

    fixing heroku push

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Resume from './components/Resume'
 import Wikitongues from './components/single/Wikitongues'
 import Poly from './components/single/Poly'
 import Amicus from './components/single/Amicus'
-import Enddash from './components/single/Enddash'
-import SamGuest from './components/single/SamGuest'
-import Airtable from './components/single/Airtable'
-import Datamonkey from './components/single/Datamonkey'
-import Quotidian from './components/single/Quotidian'
-import DV01 from './components/single/DV01'
-import Matte from './components/single/Matte'
-import NationalSawdust from './components/single/NationalSawdust'
-import VisionIntoArt from './components/single/VisionIntoArt'
-import Matchbook from './components/single/Matchbook'
-import MichelleBogre from './components/single/MichelleBogre'
-import MeAndYou from './components/single/MeAndYou'
+// import Enddash from './components/single/Enddash'
+// import SamGuest from './components/single/SamGuest'
+// import Airtable from './components/single/Airtable'
+// import Datamonkey from './components/single/Datamonkey'
+// import Quotidian from './components/single/Quotidian'
+// import DV01 from './components/single/DV01'
+// import Matte from './components/single/Matte'
+// import NationalSawdust from './components/single/NationalSawdust'
+// import VisionIntoArt from './components/single/VisionIntoArt'
+// import Matchbook from './components/single/Matchbook'
+// import MichelleBogre from './components/single/MichelleBogre'
+// import MeAndYou from './components/single/MeAndYou'
 import Gallery from './components/Gallery'
 import More from './components/single/More'
 import './css/App.css';
@@ -46,18 +46,6 @@ class App extends Component {
           <Route path="/wikitongues" component={Wikitongues}/>
           <Route path="/poly" component={Poly}/>
           <Route path="/amicus" component={Amicus}/>
-          <Route path="/enddash" component={Enddash}/>
-          <Route path="/sam-guest" component={SamGuest}/>
-          <Route path="/airtable" component={Airtable}/>
-          <Route path="/datamonkey" component={Datamonkey}/>
-          <Route path="/quotidian-ventures" component={Quotidian}/>
-          <Route path="/dv01" component={DV01}/>
-          <Route path="/matte" component={Matte}/>
-          <Route path="/national-sawdust" component={NationalSawdust}/>
-          <Route path="/visionIntoArt" component={VisionIntoArt}/>
-          <Route path="/matchbook" component={Matchbook}/>
-          <Route path="/michelle-bogre" component={MichelleBogre}/>
-          <Route path="/me-and-you" component={MeAndYou}/>
           <Route path="/more" component={More}/>
           <Route exact path="/" component={Gallery}/>
         </div>
--- src/components/single/Poly.js
+++ src/components/single/Poly.js
@@ -1,13 +1,9 @@
 import React from 'react';
-// import { Component } from 'react';
 import SingleHead from '../SingleHead'
 import ProgressiveImage from 'react-progressive-image'
 import {
-  // BrowserRouter as Router,
-  // Route,
   Link
 } from 'react-router-dom'
-// <p>I built our first prototype in Ember.js and Firebase, and had the opportunity to demonstrate it at the New York Tech Meetup. At that moment, the core functionalities were designed, and the behavior of the app had shaped up.</p>
 
 export default class Poly extends React.Component {
   componentDidMount() {
@@ -27,8 +23,6 @@ export default class Poly extends React.Component {
               {image:"media/poly/interface/mobile.jpg",placeholder:"placeholder/poly/interface/mobile.jpg",caption:"Poly needs to be accessible to people all over the world."},
               {image:"media/poly/interface/process3.jpg",placeholder:"placeholder/poly/interface/process3.jpg",caption:"For all of the simplicity from the user's side, Poly has a lot of complexity."},
               {image:"media/poly/logo/poster.jpg",placeholder:"placeholder/poly/logo/poster.jpg",caption:"The blue macaw Poly represents hope and determination in the face of adversity"},
-              // {image:"media/poly/logo/logomark.jpg",placeholder:"placeholder/poly/logo/logomark.jpg",caption:"The Poly Logomark"},
-              // {image:"media/poly/logo/banner.jpg",placeholder:"placeholder/poly/logo/banner.jpg",caption:"Publishing banner"},
               ]}
             />
 
